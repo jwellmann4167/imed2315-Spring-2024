5be21f6e9edbda75ba40cd60bb191a61c04db776
Jennifer Wellmann <122864685+jwellmann4167@users.noreply.github.com>
2024-02-25 00:08:57 -0600

    search bar

--- js/snazzymenu.js
+++ js/snazzymenu.js
@@ -155,7 +155,7 @@
         // Add unique class to mega menu columns
         const addColClasses = () => {
             if (settings.setColumnClasses) {
-                
+
                 const colTitle = menu.querySelectorAll('.column-title');
 
                 colTitle.forEach((col, i) => {
@@ -163,7 +163,7 @@
 
                     const a = col.querySelector('a');
                     const img = document.createElement('img');
-                    
+
                     img.src = settings.setImages[i];
                     img.setAttribute('alt', 'Navigation image');
 
@@ -172,7 +172,7 @@
                     }
 
                     col.insertBefore(img, a);
-                    
+
                 });
 
 
@@ -195,8 +195,8 @@
             });
 
         }
-        
-        
+
+
         // Close Menu
         const closeMenu = () => {
             const menuBtn = document.querySelector('.menu-btn');
@@ -209,17 +209,17 @@
                 if (settings.noControls) {
                     li.classList.remove('active');
                 }
-                
+
             });
 
             subMenu.forEach(sub => {
                 sub.style.height = '0px';
             });
-                
+
             closeOverlay.classList.remove('active');
             nav.classList.remove('scroll');
 
-            closeOverlay.addEventListener('click', function() {
+            closeOverlay.addEventListener('click', function () {
                 closeMenu();
             });
 
@@ -241,13 +241,13 @@
             [...topListItems].forEach(li => {
                 const topLinks = li.firstElementChild;
 
-                
+
                 topLinks.addEventListener('click', (e) => {
-                    
+
                     if (li.classList.contains('menu-item-has-children')) {
                         e.preventDefault();
                     }
-                    
+
                     for (let sibling of e.target.parentElement.parentElement.children) {
                         sibling.classList.remove('active');
                         e.target.parentElement.classList.add('active');
@@ -275,18 +275,18 @@
             nav.after(closeDiv);
 
             const cta = nav.querySelector('.cta');
-            const menuBtn = nav.querySelector('.menu-btn');  
+            const menuBtn = nav.querySelector('.menu-btn');
 
             // Add click-to-call link
             if (settings.phoneBtn) {
                 const callBtn = document.createElement('div');
                 callBtn.innerHTML = `<a href="tel:` + settings.phoneBtn + `" class="call-btn" aria-label="Call"><svg fill="currentColor" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path clip-rule="evenodd" fill-rule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"></path></svg> <span>` + settings.phoneLabel + `</span></a>`;
-                
+
                 cta.insertBefore(callBtn, menuBtn);
             }
 
-             // Add location link
-             if (settings.locationBtn) {
+            // Add location link
+            if (settings.locationBtn) {
                 const locationBtn = document.createElement('div');
                 locationBtn.innerHTML =
                     `<a href="` + settings.locationBtn + `" class="location-btn" target="_blank" aria-label="Location"><svg fill="currentColor" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path clip-rule="evenodd" fill-rule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z"></path></svg> <span>` + settings.locationLabel + `</span></a>`;
@@ -321,10 +321,10 @@
             [...topListItems].forEach(li => {
 
                 const childSub = li.querySelector('.sub-menu');
-                if ( li.contains(childSub) ) {
+                if (li.contains(childSub)) {
                     childSub.classList.add('mega-menu');
                 }
-                
+
             });
 
 
@@ -341,7 +341,7 @@
 
             // Expands dropdown menu on click
             const toggle = menu.querySelectorAll('.toggle');
-            
+
             toggle.forEach(btn => {
                 btn.addEventListener('click', (e) => {
                     e.preventDefault();
@@ -370,23 +370,23 @@
                 nav.classList.add('no-controls');
                 hasSub.forEach(li => {
                     li.classList.remove('active');
-                    
+
                     li.addEventListener('click', () => {
                         const closeOverlay = document.querySelector('.close-overlay');
                         closeOverlay.classList.add('active');
                     });
-                    
+
                 });
 
-                
+
             }
         }
-        
-        
+
+
 
 
         const checkWidth = () => {
-            
+
             const browserWidth = window.innerWidth;
 
             if (browserWidth < settings.breakpoint) {
@@ -400,7 +400,7 @@
                 nav.classList.add('desktop');
 
             }
-           
+
         }
 
         const windowResize = () => {
@@ -449,7 +449,7 @@
             // Merge user options with defaults
             settings = extend(defaults, options || {});
 
-            
+
             addElements();
             navSetup();
             openMenu();
